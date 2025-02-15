baece25f814f606970a469906b9cd9713f00851b
Alelovv <alepavlovv45@gmail.com>
2025-02-15 14:43:28 +1000

    Add files via upload

--- lab0/main.c
+++ lab0/main.c
@@ -1,10 +1,36 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
-	printf("Hello World");
+	double a, b, c;
+	double x1, x2, d;
+	
+	a = 1.2;
+	b = 10.5;
+	c = 3.3;
+	
+	d = pow(b, 2)- 4 * a * c;
+	printf("d = %lf \n", d);
+	
+	if (d > 0){
+	x1 = -b + sqrt(d) / (2 * a);
+	x2 = -b - sqrt(d) / (2 * a);
+	
+	printf("x1 = %lf \n", x1);
+	printf("x2 = %lf \n", x2);
+	}
+	
+	if (d ==0){
+	x1 = -b + sqrt(d) / (2 * a);
+	
+	printf("x1 = %lf \n", x1);
+		
+	}
+	
+	if (d <0){
+		printf("Kornei Net");
+	}
+	
 	return 0;
 }
-
