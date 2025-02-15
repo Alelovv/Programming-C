0bd7c7ec6b317fa4129d2870a6ce83aa2b975a7a
Alelovv <alepavlovv45@gmail.com>
2025-02-15 14:55:00 +1000

    Add files via upload

--- lab0/main.c
+++ lab0/main.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 #include <math.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 int main(int argc, char *argv[]) {
 	double a, b, c;
 	double x1, x2, d;
-	
+/*	
 	a = 1.2;
 	b = 10.5;
 	c = 3.3;
+*/
+	printf("Vvedite a:");
+	scanf("%lf", &a);
+	printf("Vvedite b:");
+	scanf("%lf", &b);
+	printf("Vvedite c:");
+	scanf("%lf", &c);
+	
 	
 	d = pow(b, 2)- 4 * a * c;
 	printf("d = %lf \n", d);
@@ -34,3 +43,4 @@ int main(int argc, char *argv[]) {
 	
 	return 0;
 }
+
