28c68ba3cd962da543ebc656f48ef77f70c697e6
Alelovv <alepavlovv45@gmail.com>
2025-03-01 20:13:58 +1000

    5

---
+++ lab5/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	int n;
+    printf("Input size of matrix (n): ");
+    scanf("%d", &n);
+	double Matrix1[n][n], Matrix2[n][n];
+	
+	return 0;
+}
