45eff33d8c1d57c4ab817b70a02e4dc966c59b11
Alelovv <alepavlovv45@gmail.com>
2025-04-01 23:59:15 +1000

    V1

--- V_labs/1/main.c
+++ V_labs/1/main.c
@@ -2,7 +2,6 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-
 bool is_vowel(char c) {
     c = tolower(c);
     return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y';
@@ -13,39 +12,73 @@ bool is_consonant(char c) {
     return isalpha(c) && !is_vowel(c);
 }
 
-int main() {
-    char filename[256];
-    printf("Введите имя файла: ");
-    scanf("%255s", filename);
+int isvowel(int c);
+int main(int argc, char **argv)
+{
+   FILE *F; int c, letter, cons, vowel, total=0;
+  while ( --argc )
+ {
+   F=fopen(argv[argc],"r");
+    total=letter=cons=vowel=0;
+    while ( (c=fgetc(F))!=EOF)
+     {
+          total++;
+          if (isalpha(c))
+          { 
+           letter++;
+           if (isvowel(c)) vowel++;
+           else cons++;
+          }
+     }
+      printf("FILE %s: total=%d letters=%d vowels=%d, cons=%d\n",argv[argc],total,letter,vowel,cons);
+  }
+    return 0;
+}
 
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Ошибка открытия файла");
-        return 1;
+int isvowel(int c)
+{
+    switch(tolower(c))
+    {
+        case 'a': 
+        case 'e':
+        case 'i': 
+        case 'o': 
+        case 'u':
+        case 'y': return 1;
+    }
+    return 0;
+}
+
+void analyze_file() {
+    char filename[256] = "exemple.txt";
+    char result_filename[256] = "result.txt";
+    
+    FILE *input_file = fopen(filename, "r");
+    if (input_file == NULL) {
+        printf("ERROR!\n");
+        return;
     }
 
-    int total_chars = 0;
-    int lines = 0;
-    int digits = 0;
-    int punct = 0;
-    int spaces = 0;
-    int letters = 0;
-    int lower = 0;
-    int upper = 0;
-    int vowels = 0;
-    int consonants = 0;
+    FILE *output_file = fopen(result_filename, "w");
+    if (output_file == NULL) {
+        printf("ERROR!\n");
+        fclose(input_file);
+        return;
+    }
+
+    int total_chars = 0, lines = 0, digits = 0, punct = 0;
+    int spaces = 0, letters = 0, lower = 0, upper = 0;
+    int vowels = 0, consonants = 0;
 
     int c;
-    while ((c = fgetc(file)) != EOF) {
-
-        if (c == EOF) break;
-        
+    while ((c = fgetc(input_file)) != EOF) {
         total_chars++;
         
         if (c == '\n') lines++;
         if (isdigit(c)) digits++;
         if (ispunct(c)) punct++;
         if (isspace(c)) spaces++;
+        
         if (isalpha(c)) {
             letters++;
             if (islower(c)) lower++;
@@ -55,19 +88,19 @@ int main() {
         }
     }
 
-    fclose(file);
+	fprintf(output_file, "Результаты анализа файла:\n");
+	fprintf(output_file, "Общее количество символов: %d\n", total_chars);
+	fprintf(output_file, "Количество строк: %d\n", lines + 1);
+	fprintf(output_file, "Количество цифр: %d\n", digits);
+	fprintf(output_file, "Количество знаков препинания: %d\n", punct);
+	fprintf(output_file, "Количество пробельных символов: %d\n", spaces);
+	fprintf(output_file, "Количество букв: %d\n", letters);
+	fprintf(output_file, "  Строчные буквы: %d\n", lower);
+	fprintf(output_file, "  Прописные буквы: %d\n", upper);
+	fprintf(output_file, "  Гласные буквы: %d\n", vowels);
+	fprintf(output_file, "  Согласные буквы: %d\n", consonants)
 
-    printf("Результаты анализа файла '%s':\n", filename);
-    printf("Общее количество символов: %d\n", total_chars);
-    printf("Количество строк: %d\n", lines + 1); 
-    printf("Количество цифр: %d\n", digits);
-    printf("Количество знаков препинания: %d\n", punct);
-    printf("Количество пробельных символов: %d\n", spaces);
-    printf("Количество букв: %d\n", letters);
-    printf("  из них строчных: %d\n", lower);
-    printf("  из них прописных: %d\n", upper);
-    printf("  из них гласных: %d\n", vowels);
-    printf("  из них согласных: %d\n", consonants);
+    fclose(input_file);
+    fclose(output_file);
 
-    return 0;
 }
