68e65a06de19a45d61976eff441fa448ed853856
Alelovv <alepavlovv45@gmail.com>
2025-05-24 10:16:57 +1000

    15

--- 15/main.c
+++ 15/main.c
@@ -2,7 +2,22 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void split(int n) {
+    if (n < 10) {
+        printf("%d", n);
+        return;
+    }
+    split(n / 10);
+    printf(" %d", n % 10); 
+}
+
 
 int main(int argc, char *argv[]) {
+	int n;
+	printf("input number:\n");
+	scanf("%d", &n);
+	split(n);
+	
 	return 0;
 }
+
