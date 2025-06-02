a488b9345114143dae685ce5f9c329749d6e02cd
Alelovv <alepavlovv45@gmail.com>
2025-06-02 23:28:35 +1000

    14

--- 14/main.c
+++ 14/main.c
@@ -2,30 +2,39 @@
 #include <setjmp.h>
 
 jmp_buf env;
+double j = 0;
 
-void fibonaci(int count, int *sum, int curr, int next) {
+void fibonaci(unsigned long long count, unsigned long long *sum, unsigned long long curr, unsigned long long next) {
     if (count <= 0) {
         longjmp(env, 1); 
     }
 
     *sum += curr;
+    j +=1;
     printf("%llu\n", curr);
+    if (j == 100){
+    	printf("Limit: %lf \n", j);
+    	longjmp(env, 1);
+	}
     fibonaci(count - 1, sum, next, curr + next);
 }
 
 int main() {
-	
-    long count;
-    printf("Input count: ");
+	unsigned long long result = 0;
+    unsigned long long count;
+    printf("Input count: \n");
     scanf("%llu", &count);
 
-    long long result = 0;
 
     if (setjmp(env) == 0) {
         fibonaci(count, &result, 1, 1);
     } else {
         printf("Result: %llu\n", result);
     }
-
     return 0;
 }
+
+//longjmp() восстанавливает среду, сохраненную последним запросом setjmp()
+//с передачей параметра __jmpb. По завершении longjmp(), выполнение программы 
+//продолжается как будто соответствующий запрос setjmp () всего лишь возвратил 
+//значение
